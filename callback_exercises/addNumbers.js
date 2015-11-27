a0298192a266d0fbb820ef3a6c31678df4be08b0
Winnie Ngo <ngowinn@gmail.com>
2015-11-27 11:53:57 -0800

    make Clock

---
+++ callback_exercises/addNumbers.js
@@ -0,0 +1,8 @@
+function addNumbers() {
+
+}
+
+
+addNumbers(0, 3, function (sum) {
+  console.log("Total Sum: " + sum);
+});
---
+++ callback_exercises/clock.js
@@ -0,0 +1,31 @@
+function Clock () {
+  // 1. Create a Date object.
+  var date = new Date();
+  // 2. Store the hours, minutes, and seconds.
+  this.currHrs = date.getHours();
+  this.currMins = date.getMinutes();
+  this.currSecs = date.getSeconds();
+  // 3. Call printTime.
+  this.printTime();
+  // 4. Schedule the tick at 1 second intervals.
+  var runningTick = this._tick.bind(this);
+  setTimeout(runningTick, 1000);
+  // setTimeout(runningTick, 1000);
+  // setTimeout(runningTick, 1000);
+}
+
+Clock.prototype.printTime = function () {
+  // Format the time in HH:MM:SS
+  // Use console.log to print it.
+  console.log(this.currHrs + ":" + this.currMins + ":" + this.currSecs);
+};
+
+Clock.prototype._tick = function () {
+  // 1. Increment the time by one second.
+  // 2. Call printTime.
+  this.currSecs += 1;
+  this.printTime();
+};
+
+var clock = new Clock();
+// clock.printTime();
